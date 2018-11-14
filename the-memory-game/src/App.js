5c347da55ccb927cbedb3902e16cecaf644a7a7a
Nesmikkuma <Nesmith05@yahoo.com>
2018-11-13 23:40:47 -0500

    shuffled pics

--- the-memory-game/src/App.js
+++ the-memory-game/src/App.js
@@ -17,34 +17,67 @@ class App extends Component {
     sailorsenshi,
     score: 0,
     topScore: 0,
-    
+    clicked: []
   };
 
   //incrementFunction
   handleIncrement = () => {
-    this.setState({ score: this.state.score + 1 });
-    if (this.state.score >= this.state.topScore) {
+    const newScore = this.state.score + 1;
+    this.setState({ score: newScore });
+    if (newScore >= this.state.topScore) {
       this.setState({ topScore: newScore });
+    } else if (this.state.clicked === "true") {
+      alert("not so fast");
     }
+
+    if (newScore === 12) {
+      alert("You win!");
+    }
+    // this.shuffleDeck();
+    
   }
+
+    
+  
 
   //clickFunction
-  handleClick = event => {
-    // event.preventDefault();
+  handleClick = (event) => {
+    event.preventDefault();
     this.handleIncrement();
+    this.shuffleDeck();
     this.clickedStat();
-    this.resetGame();
-
-    //if you click on a picture, you get a point
+   
     //if you click on the same picture twice, you lose and reset game.
+    if (this.state.score >= 12) {
+      this.setState({ topScore: this.state.score })
+      this.resetGame();
+    }
+    if(sailorsenshi.clicked === false) {
+      alert("woo");
+      return sailorsenshi.clicked = true;
 
-  }
+    }
+
+  };
+
   //shuffleFunction
-  //
+  shuffleDeck = sailorsenshi => {
+    this.state.sailorsenshi.sort(() => Math.random() - 0.5)
+  }
+  
   //change clicked stat
-  clickedStat = clicked => {
-    const sailorsenshi = this.state.sailorsenshi.filter(scout => scout.clicked !== clicked);
-    this.setState({ sailorsenshi })
+  clickedStat = sailorsenshi => {
+    // return sailorsenshi.clicked === "true";
+    // if (sailorsenshi.clicked === "true") {
+
+    // }
+    // if (this.state.clicked.indexOf(id) === -1) {
+    //   this.handleIncrement();
+    //   this.setState({ clicked: this.state.clicked.concat(id)})
+    // } else {
+    //   this.resetGame();
+    // }
+    
   }
 
   //resetFunction
@@ -52,10 +85,13 @@ class App extends Component {
     // if score = 12, reset game
     //if you press the same card again, reset game
     if (this.state.score > this.state.topScore) {
-      this.setState({ topScore: this.state.score })
+      this.setState({ 
+        topScore: this.state.score, 
+        clicked: false,
+        score: 0
+      });
     }
-    this.setState ({ score: 0});
-    alert(`Game Over :( \nscore: ${this.state.score}`);
+    
   }
 
   render() {
@@ -66,8 +102,9 @@ class App extends Component {
           {/* <container> */}
             {this.state.sailorsenshi.map(sailorscout => (
               <SailorSenshi
-                id={sailorscout.id}
+                key={sailorscout.id}
                 image={sailorscout.image}
+                clicked={sailorscout.clicked}
                 // onClick={this.handleClick}
                 handleClick={this.handleClick}
               />
--- the-memory-game/src/components/SailorSenshi/SailorSenshi.js
+++ the-memory-game/src/components/SailorSenshi/SailorSenshi.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./SailorSenshi.css";
 
 const SailorSenshi = props => (
-    <div onClick={() => props.handleClick()}className = "container">
+    <div onClick={props.handleClick} className = "container">
         <div className="img-container">
             <img alt={props.name} src={props.image} />
         </div>
